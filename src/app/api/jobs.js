0256755fdd573e2b9b1a1a5dd738d279bb5469a9
AbhishekVerma128 <vermaabhishek128@gmail.com>
2025-11-08 19:40:32 +0530

    ok

--- src/app/api/jobs.js
+++ src/app/api/jobs.js
@@ -1,6 +1,6 @@
 export async function getJobHistory(){
     try{
-        const res = await fetch('http://localhost:5000/job-history');
+        const res = await fetch('http://127.0.0.1:5000/api/job-history');
         if (!res.ok) {
             throw new Error('Failed to fetch job history');
         }
